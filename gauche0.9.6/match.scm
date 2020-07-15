f43d720dd579bbf25dfd6b79069b541d704453ac
fjt <fthibault1969@gmail.com>
2020-07-15 09:19:47 -0400

    get match to run in gosh

---
+++ gauche0.9.6/match.scm
@@ -0,0 +1,7 @@
+(define-library (gauche0.9.6  match)
+  (export match match-lambda match-lambda* match-let match-letrec match-let*)
+  (import (scheme base))
+  (include "match/match.scm"))
+
+;;; (import (gauche0.9.6 match))
+;;; (import (scheme base))
