2ba267cacaf31089a6a1debc45f9963b0518c811
fjt <fthibault1969@gmail.com>
2020-08-15 05:58:18 -0400

    add syntax exports to Foment, still broken

--- srfi/foment0.4/match.scm
+++ srfi/foment0.4/match.scm
@@ -1,5 +1,6 @@
 (define-library (foment0.4  match)
-		(export match match-lambda match-lambda* match-let match-letrec match-let*)
+		(export match match-lambda match-lambda* match-let match-letrec match-let*
+			___ ..1 ..= ..* *** ? $ struct object get!)
 		(import (scheme base))
 		(include "../match/match.scm"))
 ;;foment -I ..
